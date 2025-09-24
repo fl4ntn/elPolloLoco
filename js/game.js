2db0a6ddfd59a76c6c9df6628c1e00fa58b31faa
fl4ntn <flynn.anton@gmx.de>
2025-09-24 09:05:29 +0200

    templates on start screen

--- js/game.js
+++ js/game.js
@@ -15,8 +15,12 @@ function init() {
 
 
 function openSettings() {
-    document.getElementById('settings_overlay').classList.remove('d_none');
-    // document.getElementById('explanation_board').classList.add('d_none');
+    // document.getElementById('settings_overlay').classList.remove('d_none');
+    document.getElementById('explanation_board').innerHTML = getSettingsOverlay();
+}
+
+function getExplanationBoard() {
+    document.getElementById('explanation_board').innerHTML = getExplanationBoardTemplate();
 }
 
 window.addEventListener('click', function(event) {
@@ -154,6 +158,82 @@ function gameOver() {
 
     }
 
+    function getExplanationBoardTemplate() {
+        return `
+            <div class="explanation_box">
+                <img class="map" src="img/map.png" alt="map">
+                <p>EXPLANATION</p>
+            </div>
+            
+            <div onclick="openSettings()" class="explanation_box">
+                <img class="map rotate" src="img/settings.png" alt="map">
+                <p>SETTINGS</p>
+            </div>
+            <div onclick="init()" class="explanation_box">
+                <img class="map" src="img/old_door.png" alt="map">
+                <p>START GAME</p>
+            </div>
+        
+        `
+    }
+    
+    function getSettingsOverlay() {
+        return `
+        <div>
+        <div class="settings">
+            <div class="settings_option">
+               
+                <div class="ticked_box">
+                    <div class="box"></div>
+                    <div class="cross">X</div>
+                </div>
+                <p class="font60">Sound</p>
+            </div>
+            <div class="settings_option">
+                
+                <div class="ticked_box">
+                    <div class="box"></div>
+                    <div class="cross">X</div>
+                </div>
+                <p class="font60">Level 1</p>
+            </div>
+            <div class="settings_option">
+                
+                <div class="ticked_box">
+                    <div class="box"></div>
+                    <div class="cross">X</div>
+                </div>
+                <p class="font60">Level 2</p>
+            </div>
+        </div>
 
+        <div class="leave_settings">
+          
+                <p onclick="init()" class="font60">Start Game</p>
+            
+           
+                <p onclick="getExplanationBoard()" class="font60">Go Back</p>
+            
+        
+        
+        
+        
+        </div>
+        </div>
+
+
+
+
+
+        
+            
+           
+        
+        
+        
+        
+        
+        `
+    }
 
 
